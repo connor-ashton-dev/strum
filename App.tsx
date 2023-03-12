7cb0c7b1f7e44ab1cafd3901efa185e05bc2fe8e
AirMormon1029 <cjconnorashton@gmail.com>
2023-03-12 17:11:02 -0600

    random commit 2

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './components/AuthProvider';
 import { NavigationContainer } from '@react-navigation/native';
 import Toast from 'react-native-toast-message';
-
 import Router from './Router';
 
 export default function App() {
