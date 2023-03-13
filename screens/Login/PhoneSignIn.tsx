6627b79e812f8c914bd59a90301bf6ebaba69cc4
AirMormon1029 <cjconnorashton@gmail.com>
2023-03-13 15:53:53 -0600

    added login elements

---
+++ screens/Login/PhoneSignIn.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PhoneSignIn = () => {
+  return (
+    <View className='flex-1 bg-theme-red'>
+      <Text>PhoneSignIn</Text>
+    </View>
+  )
+}
+
+export default PhoneSignIn
