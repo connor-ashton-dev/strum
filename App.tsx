827009cab93c84bdfb681ecfc630a567c5c4893d
Connor Ashton <cjconnorashton@gmail.com>
2023-04-24 19:11:42 -0600

    testing out git fugitive

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { AuthProvider } from "./components/AuthProvider";
-import { NavigationContainer } from "@react-navigation/native";
-import Toast from "react-native-toast-message";
-import Router from "./Router";
+import { StatusBar } from 'expo-status-bar';
+import { AuthProvider } from './components/AuthProvider';
+import { NavigationContainer } from '@react-navigation/native';
+import Toast from 'react-native-toast-message';
+import Router from './Router';
 
 export default function App() {
   return (
     <NavigationContainer>
       <AuthProvider>
         <Router />
+{/*ADDED A SILLY COMMENT HEE HEE*/}
         <StatusBar style="auto" />
         <Toast />
       </AuthProvider>
